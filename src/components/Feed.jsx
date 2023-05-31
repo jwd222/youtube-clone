e342a68981ef721eedeb88385aff6104b9d0dde0
jwd222 <sji.2.sami@gmail.com>
2023-06-01 01:12:34 +0600

    API used and videos on the way

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import { useState, useEffect} from 'react';
 import { Box, Stack, Typography } from '@mui/material';
+import { Sidebar, Videos} from './';
+
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const Feed = () => {
+
+  const [selectedCategory, setSelectedCategory ] = useState('New');
+  const [videos, setVideos] = useState([]);
+ 
+  useEffect(() => {
+    fetchFromAPI(`search?part&snipppet=&q={selectedCategory}`)
+    .then((data) => setVideos(data.items))
+  }, [selectedCategory]);
+
   return (
     <Stack sx={{
       flexDirection: { sx: "column", md: "row"}
@@ -12,7 +24,10 @@ const Feed = () => {
         borderRight: '1px solid #3d3d3d',
         px: {sx: 0, md: 2}
       }}>
-        Sidebar
+        <Sidebar
+          selectedCategory = {selectedCategory}
+          setSelectedCategory = {setSelectedCategory}
+        />
 
         <Typography 
           className='copyright' 
@@ -21,6 +36,23 @@ const Feed = () => {
           >
           Copyright 2023 Shah Jawad Islam
         </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: 'auto',
+        height: '90vh', flex: 2}}>
+        <Typography variant='h4' 
+        fontWeight='bold' marginBottom={2} sx={{
+          color:'white'  
+        }} 
+        >
+          {selectedCategory}  <span style={{
+            color: "#F31503"
+          }}>
+            videos
+          </span>
+        </Typography>
+
+        <Videos videos={videos}/>
       </Box>
       
     </Stack>
