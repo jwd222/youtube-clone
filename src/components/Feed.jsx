d7a5a1fe3a510acd39de272424b7532a338e7fde
jwd222 <sji.2.sami@gmail.com>
2023-05-31 22:19:07 +0600

    Working on Feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useState, useEffect} from 'react';
+import { Box, Stack, Typography } from '@mui/material';
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack sx={{
+      flexDirection: { sx: "column", md: "row"}
+    }}>
+      <Box sx={{
+        height: { sx: 'auto', md: '92vh' },
+        borderRight: '1px solid #3d3d3d',
+        px: {sx: 0, md: 2}
+      }}>
+        Sidebar
+
+        <Typography>
+          Copyright Shah Jawad Islam
+        </Typography>
+      </Box>
+      
+    </Stack>
   )
 }
 
