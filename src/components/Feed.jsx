0e4a7f9e6787a21d1640a13d7dcea812ddd79174
jwd222 <sji.2.sami@gmail.com>
2023-06-01 18:59:43 +0600

    API working and able to fetch videos for each category

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -7,12 +7,12 @@ import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const Feed = () => {
 
-  const [selectedCategory, setSelectedCategory ] = useState('New');
-  const [videos, setVideos] = useState([]);
- 
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([]) 
+
   useEffect(() => {
-    fetchFromAPI(`search?part&snipppet=&q={selectedCategory}`)
-    .then((data) => setVideos(data.items))
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+    .then(( data ) => setVideos(data.items))
   }, [selectedCategory]);
 
   return (
@@ -24,10 +24,10 @@ const Feed = () => {
         borderRight: '1px solid #3d3d3d',
         px: {sx: 0, md: 2}
       }}>
-        <Sidebar
-          selectedCategory = {selectedCategory}
-          setSelectedCategory = {setSelectedCategory}
-        />
+        <Sidebar 
+          selecetedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+          />
 
         <Typography 
           className='copyright' 
@@ -41,14 +41,12 @@ const Feed = () => {
       <Box p={2} sx={{ overflowY: 'auto',
         height: '90vh', flex: 2}}>
         <Typography variant='h4' 
-        fontWeight='bold' marginBottom={2} sx={{
+          fontWeight='bold' marginBottom={2} sx={{
           color:'white'  
         }} 
         >
-          {selectedCategory}  <span style={{
-            color: "#F31503"
-          }}>
-            videos
+          {selectedCategory} <span style={{ color: "#F31503"}}>
+              videos
           </span>
         </Typography>
 
--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,20 +1,18 @@
 import axios from "axios";
 
-const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
-
 const options = {
-  url: BASE_URL,
+  //url: 'https://youtube-v31.p.rapidapi.com/search',
   params: {
     maxResults: '50'
   },
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': 'de9c53a4damsh6a4d399d7afc81ap154bf6jsn2d446e19b026',
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
   }
 };
 
-export const fetchFromAPI = async(url) => {
-  const { data } = await axios.get(`${BASE_URL}/${url}`, 
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${'https://youtube-v31.p.rapidapi.com'}/${url}`, 
   options);
 
   return data;
